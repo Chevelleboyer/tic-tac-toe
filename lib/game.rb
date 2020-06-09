e535c2222bc67397520ab00f08befc02b16c7e94
Chevelle Boyer <chevelleboyer@gmail.com>
2020-06-09 09:14:03 -0700

    Add winner method

--- lib/game.rb
+++ lib/game.rb
@@ -2,9 +2,14 @@ require_relative 'board'
 
 class Game
 
-	attr_accessor :board
+	attr_accessor :board, :winner
 
 	def initialize(board=nil)
 		@board = board
+		@winner
+	end
+
+	def winner
+		@winner
 	end
 end
