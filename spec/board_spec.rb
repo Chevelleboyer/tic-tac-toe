18b16360d233d4ba9b10c9c990f3c8ce90a0d1b9
Chevelle Boyer <chevelleboyer@gmail.com>
2020-06-09 09:44:01 -0700

    Check existance of board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,12 @@ require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
 
-  it 'needs covered with tests' do
-    fail 'See spec/board_spec.rb'
+  # it 'needs covered with tests' do
+  #   fail 'See spec/board_spec.rb'
+  # end
+
+  it "exists" do
+  	Board.new
   end
 
 end
